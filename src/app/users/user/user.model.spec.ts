f7e812540b577452ae8238901c6e6f1a75606b65
Matt Daisley <mattdaisley@gmail.com>
2016-08-10 19:20:10 -0600

    update user modal test

--- src/app/users/user/user.model.spec.ts
+++ src/app/users/user/user.model.spec.ts
@@ -1,10 +1,22 @@
 /* tslint:disable:no-unused-variable */
 
-import { addProviders, async, inject } from '@angular/core/testing';
 import { User } from './user.model';
 
-describe('User', () => {
-  it('should create an instance', () => {
-    // expect(new User()).toBeTruthy();
+describe('User Modal', () => {
+
+  it('has a name', () => {
+    let user: User = { id: 1, email: 'email@email.com', name: 'name of user', password: '123', permissions: 'none' };
+    expect(user.name).toEqual('name of user');
   });
+
+  it('has an email', () => {
+    let user: User = { id: 1, email: 'email@email.com', name: 'name of user', password: '123', permissions: 'none' };
+    expect(user.email).toEqual('email@email.com');
+  });
+
+  it('has an id', () => {
+    let user: User = { id: 1, email: 'email@email.com', name: 'name of user', password: '123', permissions: 'none' };
+    expect(user.id).toEqual(1);
+  });
+
 });
